382f509c5b5ca7739440cf2cdb7c573eb669c810
Yash Goyal <141110450+0073212@users.noreply.github.com>
2025-04-30 09:22:59 +0530

    Delete Development directory
    because i make a new directory where all the files with backend is available

--- Development/Gym owner/dashboard.js
+++
@@ -1,8 +0,0 @@
-// Sidebar Collapse Toggle
-const sidebar = document.querySelector('.sidebar');
-const collapseBtn = document.querySelector('.collapse-btn');
-
-collapseBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-});
-
--- Development/Gym owner/manage-client.js
+++
@@ -1,7 +0,0 @@
-// Sidebar toggle functionality
-const collapseBtn = document.querySelector('.collapse-btn');
-const sidebar = document.querySelector('.sidebar');
-
-collapseBtn.addEventListener('click', () => {
-    sidebar.classList.toggle('collapsed');
-});
