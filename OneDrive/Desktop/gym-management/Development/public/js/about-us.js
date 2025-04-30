7483b1ea6b45e7b1755c688095c6e29474b9f284
Yash Goyal <141110450+0073212@users.noreply.github.com>
2025-04-30 14:50:58 +0530

    Delete OneDrive/Desktop/gym-management directory i make another directory

--- OneDrive/Desktop/gym-management/Development/public/js/about-us.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById('menu-toggle');
-const navLinks = document.querySelector('.nav-links');
-
-toggleBtn.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
-
